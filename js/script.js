d17a15b863f60907236546e7262d8e3d72c7c829
Guilherme Benicio <guilhermebenicio18102003@gmail.com>
2023-02-17 10:15:52 -0300

    javascript adicionado na galeria de imagens

--- js/script.js
+++ js/script.js
@@ -47,3 +47,22 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+// Galeria de Bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+  console.log(img);
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
