150a8a8dc215883ad9b427d232d30e30654df182
Guilherme Benicio <guilhermebenicio18102003@gmail.com>
2023-02-17 09:29:30 -0300

    JavaScript adicionado

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// Ativar links do menu
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+
+  url.includes();
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
+
+// Ativar itens do Orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
